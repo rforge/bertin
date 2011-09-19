2bcc74036fb84126bbdc393ac921d25fc9ebaad8
gsawitzki <gsawitzki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-19 19:39:42 +0000

    imagem, bertinrect par(pin) unified
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bertin@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bertinrect.R
+++ pkg/R/bertinrect.R
@@ -1,8 +1,8 @@
+#$HeadURL$
 #$Id$
 #$Revision$
 #$Date$
 #$Author$
-
 
 # rect function (xleft, ybottom, xright, ytop, density = NULL, angle = 45, 
 #    col = NA, border = NULL, lty = par("lty"), lwd = par("lwd"), 
@@ -10,105 +10,25 @@
 #
 # aspect ratio ok
 # single row: not ok, eg. bertinrect(Hotel[19,]) bertinrect(Hotel[,12])
-bertinrect0 <- function(x,main,
+bertinrect <- function(z, main, 
 	sepwd=0.05,
-	mar= c(1,1,6,4)+0.1,
+	pars,  # mar= c(1,1,6,4)+0.1,
 	...){
 #$Revision$
 	# [i,j] bottom left is at user coordinates (i,j)
 	# sepwd is internal margin
-	if (missing(main)) {main <- deparse(substitute(x))}	
-	x <- as.matrix(x) #! support lists and data frames as well
-
-	oldpar <- par(no.readonly = TRUE)
-	#on.exit(par(oldpar))
-
-    par(mar=mar)
- #cat("mar  set up.pin:",par("pin")," mai:",par("mai"))
- #cat(" usr:",par("usr"),"\n")
-	parasp(t(x))#par(pin=c(3,4))
- #cat("pin  set up.pin:",par("pin")," mai:",par("mai"))
- # cat(" usr:",par("usr"),"\n")
-
-	plot(c(1, ncol(x)+1), c(1, nrow(x)+1), 
-	main=main,type= "n", xlab="", ylab="", axes=FALSE,mar=mar,...)
-#cat("Plot set up.pin:",par("pin")," mar:",par("mar"))
-#cat(" usr:",par("usr"),"\n")
-
-
-
-	#! improve. use scaling as in plot.window
-	ranges <- apply(x,1,range,finite=TRUE) # transposed
-	ranges[1,] <- pmin(0,ranges[1,]) #tack at zero
-	ranges[2,] <- pmax(0,ranges[2,]) #tack at zero
-	scale <- ranges[2,] - ranges [1,]
-	scale[] <- ifelse(scale[]==0,0.1,scale[])
-	scale[!is.finite(scale[] )] <- 0.1 	# fix zero scales
-	scale[scale[]==0] <- 0.1
-	#add some margin. ! improve. use scaling as in plot.window
-	scale <- (1-2*sepwd)/scale
-	zeroline <- -ranges[1,]*scale
-
-	xleft <- matrix((1:ncol(x)), nrow(x), ncol(x), byrow=TRUE)+sepwd
-	xright <- xleft+1-2*sepwd
-	xbottom <-  1+nrow(x)-(matrix((1:nrow(x)),nrow(x),ncol(x))) + 
-		sepwd +zeroline #box zero line
-	xtop <- x*scale+xbottom
-
-if (any(ranges[2,]==0)){
-	abline(h= xbottom[ranges[2,]==0],lty=3,col="gray")}
-	rect(xleft,xbottom,xright,xtop,...)
-#cat("Rect set up.pin:",par("pin"))
-#cat(" usr:",par("usr"),"\n")
-
-	#textnames(x)
-       if (!is.null(colnames(x))){ 
-       	for (col in (1:dim(x)[2])) 
-       		text(col, par("usr")[4], colnames(x)[col], 
-  #          pos = 3, xpd = NA, offs = 1, srt = 90, cex=0.6)}
-             adj=c(0,1),xpd = NA,  srt = 90, cex=0.6)}
-       if (!is.null(rownames(x))) {
-       	r <- par("usr")[2] #right
-       	for (row in (1:dim(x)[1])) 
-       		text(r, nrow(x)-row+1.4, rownames(x)[row], 
-            pos = 4, xpd = NA, offs = 0, srt = 0, cex=0.6)
-            
-   if (!all(is.finite(x))) {
-   	badpos <- !is.finite(x)
-   	xbottom <-  1+nrow(x)-(matrix((1:nrow(x)),nrow(x),ncol(x))) + sepwd #box zero line
-   	text(xleft[badpos]+0.4,xbottom[badpos],labels=x[badpos], pos=3, offs=0.5,col="red",cex=0.6)
-   }
-} 
-}
-
-#$Id$
-#$Revision$
-#$Date$
-#$Author$
-
-
-# rect function (xleft, ybottom, xright, ytop, density = NULL, angle = 45, 
-#    col = NA, border = NULL, lty = par("lty"), lwd = par("lwd"), 
-#    ...) 
-#
-# aspect ratio ok
-# single row: not ok, eg. bertinrect(Hotel[19,]) bertinrect(Hotel[,12])
-bertinrect <- function(x,main,
-	sepwd=0.05,
-	mar= c(1,1,6,4)+0.1,
-	...){
-#$Revision$
-	# [i,j] bottom left is at user coordinates (i,j)
-	# sepwd is internal margin
-	if (missing(main)) {main <- deparse(substitute(x))}	
-	x <- as.matrix(x) #! support lists and data frames as well
-	nrow <- nrow(x)
-	ncol <- ncol(x)
-    strwrow <- max(strwidth(rownames(x),"inch"))
-    strcol <- max(strwidth(colnames(x),"inch"))
+	if (missing(main)) {main <- deparse(substitute(z))}	
+	z <- as.matrix(z) #! support lists and data frames as well
+	nrow <- nrow(z)
+	ncol <- ncol(z)
+	if (missing(pars)){
+    strwrow <- max(strwidth(rownames(z),"inch"))
+    strcol <- max(strwidth(colnames(z),"inch"))
+    chwidth <- par("cin")[1]
 	mai <- par("mai")
-	mai[3]<-strcol
-	mai[4]<-strwrow
+	mai[2]<- chwidth
+	mai[3]<-strcol + 2*chwidth
+	mai[4]<-strwrow + 2*chwidth
 	par(mai=mai)
 	plot.new()
 	#adjust plot region
@@ -117,29 +37,32 @@ bertinrect <- function(x,main,
 	aspx <- nrow/ncol
 	if (aspp > aspx) {pin[2]<-pin[2]/aspp*aspx} else {pin[1] <- pin[1]/aspx*aspp}
 	par(pin=pin)
-	plot.window(c(1, ncol(x)+1), c(1, nrow(x)+1),xaxs="i",yaxs="i", asp=1)
+	} else {
+		plot.new()
+		if(!is.null(pars)) { par(pars)}
+	}
+	
+#	}
+#	else 
+#	{
+#		plot.new()
+#		if(!is.null(pars)) { par(pars)}
+#	}
 
-#	plot.window(c(1, ncol(x)+1), c(1, nrow(x)+1),xaxs="i",yaxs="i", asp=1)
-p <-par("din","fin","pin","mai")
-p;p$pin[1]+ p$mai[2]+p$mai[4];p$pin[2]+ p$mai[1]+p$mai[3]
-par("usr")
+
+	plot.window(c(1, ncol(z)+1), c(1, nrow(z)+1),xaxs="i",yaxs="i", asp=1)
+	p <-par("cin","din","fin","pin","plt","mai", "mar","usr")
 #	oldpar <- par(no.readonly = TRUE)
 	#on.exit(par(oldpar))
 #    par(mar=mar)
- #cat("mar  set up.pin:",par("pin")," mai:",par("mai"))
- #cat(" usr:",par("usr"),"\n")
- #	parasp(t(x))#par(pin=c(3,4))
- #cat("pin  set up.pin:",par("pin")," mai:",par("mai"))
- # cat(" usr:",par("usr"),"\n")
+ #	parasp(t(z))#par(pin=c(3,4))
 
-#	plot(c(1, ncol(x)+1), c(1, nrow(x)+1), 
+#	plot(c(1, ncol(z)+1), c(1, nrow(z)+1), 
 #	main=main,type= "n", xlab="", ylab="", axes=FALSE,mar=mar,...)
-#cat("Plot set up.pin:",par("pin")," mar:",par("mar"))
-#cat(" usr:",par("usr"),"\n")
 
-	#usrold <- par(usr= c(1, (ncol(x)+1)*(1+2*sepwd), 1, (nrow(x)+1)*(1+2*sepwd)) )
+	#usrold <- par(usr= c(1, (ncol(z)+1)*(1+2*sepwd), 1, (nrow(z)+1)*(1+2*sepwd)) )
 	#! improve. use scaling as in plot.window
-	ranges <- apply(x,1,range,finite=TRUE) # transposed
+	ranges <- apply(z,1,range,finite=TRUE) # transposed
 	ranges[1,] <- pmin(0,ranges[1,]) #tack at zero
 	ranges[2,] <- pmax(0,ranges[2,]) #tack at zero
 	scale <- ranges[2,] - ranges [1,]
@@ -150,11 +73,11 @@ par("usr")
 	scale <- (1-2*sepwd)/scale
 	zeroline <- -ranges[1,]*scale
 
-	xleft <- matrix((1:ncol(x)), nrow(x), ncol(x), byrow=TRUE)+sepwd
+	xleft <- matrix((1:ncol(z)), nrow(z), ncol(z), byrow=TRUE)+sepwd
 	xright <- xleft+1-2*sepwd
-	xbottom <-  1+nrow(x)-(matrix((1:nrow(x)),nrow(x),ncol(x))) + 
+	xbottom <-  1+nrow(z)-(matrix((1:nrow(z)),nrow(z),ncol(z))) + 
 		sepwd +zeroline #box zero line
-	xtop <- x*scale+xbottom
+	xtop <- z*scale+xbottom
 
 if (any(ranges[2,]==0)){
 	abline(h= xbottom[ranges[2,]==0],lty=3,col="gray")}
@@ -162,23 +85,25 @@ if (any(ranges[2,]==0)){
 #cat("Rect set up.pin:",par("pin"))
 #cat(" usr:",par("usr"),"\n")
 
-	#textnames(x)
+	#textnames(z)
    #          pos = 3, xpd = NA, offs = 1, srt = 90, cex=0.6)}
-      if (!is.null(colnames(x))){ 
-       	for (col in (1:dim(x)[2])) 
-       		text(col+0.2, par("usr")[4]+0.5, colnames(x)[col], 
+      if (!is.null(colnames(z))){ 
+       	for (col in (1:dim(z)[2])) 
+       		text(col+0.2, par("usr")[4]+0.5, colnames(z)[col], 
              adj=c(0,1),xpd = NA, offset = 4, srt = 90, cex=0.6)}
-       if (!is.null(rownames(x))) {
+       if (!is.null(rownames(z))) {
        	r <- par("usr")[2] #right
-       	for (row in (1:dim(x)[1])) 
-       		text(r, nrow(x)-row+1.4, rownames(x)[row], 
+       	for (row in (1:dim(z)[1])) 
+       		text(r, nrow(z)-row+1.4, rownames(z)[row], 
             pos = 4, xpd = NA, offset = 0.5, srt = 0, cex=0.6)
             
-   if (!all(is.finite(x))) {
-   	badpos <- !is.finite(x)
-   	xbottom <-  1+nrow(x)-(matrix((1:nrow(x)),nrow(x),ncol(x))) + sepwd #box zero line
-   	text(xleft[badpos]+0.4,xbottom[badpos],labels=x[badpos], pos=3, offs=0.5,col="red",cex=0.6)
+   if (!all(is.finite(z))) {
+   	badpos <- !is.finite(z)
+   	xbottom <-  1+nrow(z)-(matrix((1:nrow(z)),nrow(z),ncol(z))) + sepwd #box zero line
+   	text(xleft[badpos]+0.4,xbottom[badpos],labels=z[badpos], pos=3, offs=0.5,col="red",cex=0.6)
    }
 } 
+ invisible(p)
 }
 
+# bertinrect(Hotel)
--- pkg/R/imagem.R
+++ pkg/R/imagem.R
@@ -3,7 +3,8 @@
 #$Revision$
 #$Date$
 #$Author$
-imagem <- 
+
+imagem33 <- 
 function (z, 
 	zlim = range(z[is.finite(z)]), 
 	xlim = c(1,ncol(z)), 
@@ -75,3 +76,208 @@ function (z,
 
 #imagem(mm, main="mm")
 #imagem(t(mm), main="t(mm)")
+
+# imagem38
+imagem38 <- 
+function (z, 
+	zlim, 
+	xlim = c(1,ncol(z)), 
+    ylim = c(1,nrow(z)), 
+    col = heat.colors(12),
+	add = FALSE, xaxs = "i", yaxs = "i", 
+	xlab, ylab,
+	main = deparse(substitute(z)),
+	pars, # mar= c(2,2,6,6)+0.1,
+	breaks, oldstyle=FALSE,
+	names=TRUE, 
+	coloffs=0.5, rowoffs=0.5, ...)
+{
+# a variant of image.default keeping matrix orientation
+
+#$Revision$
+#! adjust calling structure with image()
+#      image.default(x, y, z, zlim, xlim, ylim, col = heat.colors(12),
+#           add = FALSE, xaxs = "i", yaxs = "i", xlab, ylab,
+#           breaks, oldstyle = FALSE, ...)
+
+#
+	if (missing(main)) {main <- deparse(substitute(z))}	
+	zi <- t(as.matrix(z)) #! support lists and data frames as well
+#	opin <- par("pin","mar")#; #on.exit(par(pin=opin))
+#	par(mar=mar)
+#	parasp(zi)
+
+#! clean up. remove x (or zi)
+	x <- as.matrix(zi) #! support lists and data frames as well
+	if (missing(zlim)) zlim <- range(zi[is.finite(zi)])
+	nrow <- nrow(x)
+	ncol <- ncol(x)
+	if (missing(pars)){
+    strwrow <- max(strwidth(rownames(x),"inch"))
+    strcol <- max(strwidth(colnames(x),"inch"))
+    chwidth <- par("cin")[1]
+	mai <- par("mai")
+	mai[2]<- chwidth
+	mai[3]<-strcol + 2*chwidth
+	mai[4]<-strwrow + 2*chwidth
+	par(mai=mai)
+#	plot.new()
+	#adjust plot region
+	pin <- par("pin")
+	aspp <- pin[2]/pin[1]
+	aspx <- nrow/ncol
+	if (aspp > aspx) {pin[2]<-pin[2]/aspp*aspx} else {pin[1] <- pin[1]/aspx*aspp}
+	par(pin=c(pin[2],pin[1]))
+#	par(pin=pin)
+	} else {
+#		plot.new()
+		if(!is.null(pars)) { par(pars)}
+	}
+
+
+	image(
+		1:nrow(zi),1:ncol(zi), zlim=zlim,
+		#xlim=xlim,
+		ylim=c(ncol(zi)+0.5,0.5), 
+		col=col, add=add, xaxs=xaxs, yaxs=yaxs,
+		xlab="", ylab="",z=zi, 
+		main=main,
+		breaks=breaks, oldstyle=oldstyle,
+#		useRaster=TRUE,	
+#Error in image.default(1:nrow(zi), 1:ncol(zi), zlim = zlim, ylim = c(ncol(zi) +  : 
+#  formal argument "useRaster" matched by multiple actual arguments
+#  ????
+#		frame.plot=FALSE,
+		...)
+
+	p <-par("cin","din","fin","pin","plt","mai", "mar","usr")
+
+# print(paste("pin:",par("pin")))
+# print(paste("usr:",par("usr")))
+
+#	image(
+#		1:ncol(z), 1:nrow(z),z=t(z),zlim=zlim,
+#		xlim=xlim,
+#		ylim=c(nrow(z)+0.5,0.5), 
+#	)
+
+	if (names) {
+		#! improve placement of names. use either nrow/ncol or par("usr")
+
+	# textnames <-	function (zi, coloffs=-1, rowoffs=NULL) {
+		# note: image interchanges rows/colums
+		for (x in (1:dim(zi)[1]) ) # column labels
+			text(x, par("usr")[4]-coloffs , rownames(zi)[x], adj=c(0,0.5),#pos=3, 
+				xpd=NA,offs= coloffs, srt=90, cex=0.6)
+		
+		r <- par("usr")[2]+rowoffs
+		for (y in (1:dim(zi)[2]))  # row labels
+			text(r, y, colnames(zi)[y], adj=c(0,0.5),#pos=4, 
+			xpd=NA, offs=rowoffs,srt=0,cex=0.6)
+#	} # textnames
+	
+#		textnames(zi,coloffs=-4,rowoffs=1)
+	} # names
+	
+	 invisible(p)
+}#imagem38
+
+imagem <- 
+function (z, 
+	zlim, 
+	xlim = c(1,ncol(z)), 
+    ylim = c(1,nrow(z)), 
+    col = heat.colors(12),
+	add = FALSE, xaxs = "i", yaxs = "i", 
+	xlab, ylab,
+	main = deparse(substitute(z)),
+	pars, # mar= c(2,2,6,6)+0.1,
+	breaks, oldstyle=FALSE,
+	names=TRUE, 
+	coloffs=0.5, rowoffs=0.5, ...)
+{
+# a variant of image.default keeping matrix orientation
+# note: image() shows matrix columns as rows, i.e. transposes
+#$Revision$
+#! adjust calling structure with image()
+#      image.default(x, y, z, zlim, xlim, ylim, col = heat.colors(12),
+#           add = FALSE, xaxs = "i", yaxs = "i", xlab, ylab,
+#           breaks, oldstyle = FALSE, ...)
+
+#
+	if (missing(main)) {main <- deparse(substitute(z))}	
+	zi <- t(as.matrix(z)) #! support lists and data frames as well
+#	opin <- par("pin","mar")#; #on.exit(par(pin=opin))
+#	par(mar=mar)
+#	parasp(zi)
+
+#! clean up. remove x (or zi)
+	z <- as.matrix(z) #! support lists and data frames as well
+	if (missing(zlim)) zlim <- range(zi[is.finite(zi)])
+	nrow <- nrow(z)
+	ncol <- ncol(z)
+	if (missing(pars)){
+    strwrow <- max(strwidth(rownames(z),"inch"))
+    strcol <- max(strwidth(colnames(z),"inch"))
+    chwidth <- par("cin")[1]
+	mai <- par("mai")
+	mai[2]<- chwidth
+	mai[3]<-strcol + 2*chwidth
+	mai[4]<-strwrow + 2*chwidth
+	par(mai=mai)
+#	plot.new()
+	#adjust plot region
+	pin <- par("pin")
+	aspp <- pin[2]/pin[1]
+	aspx <- nrow/ncol
+	if (aspp > aspx) {pin[2]<-pin[2]/aspp*aspx} else {pin[1] <- pin[1]/aspx*aspp}
+	par(pin=pin)
+	} else {
+		#plot.new()
+		if(!is.null(pars)) { par(pars)}
+	}
+	
+
+	image.default(
+		1:nrow(zi),1:ncol(zi), zlim=zlim,
+		#xlim=xlim,
+		ylim=c(ncol(zi)+0.5,0.5), 
+		col=col, add=add, xaxs=xaxs, yaxs=yaxs,
+		xlab="", ylab="",z=zi, xaxt="n",  yaxt="n",
+		main=main,
+		breaks=breaks, oldstyle=oldstyle,
+#		useRaster=TRUE,	
+#Error in image.default(1:nrow(zi), 1:ncol(zi), zlim = zlim, ylim = c(ncol(zi) +  : 
+#  formal argument "useRaster" matched by multiple actual arguments
+#  ????
+#		frame.plot=FALSE,
+#	asp=1,
+		...)
+
+	p <-par("cin","din","fin","pin","plt","mai", "mar","usr")
+
+# print(paste("pin:",par("pin")))
+# print(paste("usr:",par("usr")))
+
+par(usr=c(1, ncol(z)+1, 1, nrow(z)+1))#,xaxs="i",yaxs="i"
+	if (names) {
+		#! improve placement of names. use either nrow/ncol or par("usr")
+
+	#textnames(z)
+   #          pos = 3, xpd = NA, offs = 1, srt = 90, cex=0.6)}
+      if (!is.null(colnames(z))){ 
+       	for (col in (1:dim(z)[2])) 
+       		text(col+0.2, par("usr")[4]+0.5, colnames(z)[col], 
+             adj=c(0,1),xpd = NA, offset = 4, srt = 90, cex=0.6)}
+       if (!is.null(rownames(z))) {
+       	r <- par("usr")[2] #right
+       	for (row in (1:dim(z)[1])) 
+       		text(r, nrow(z)-row+1.4, rownames(z)[row], 
+            pos = 4, xpd = NA, offset = 0.5, srt = 0, cex=0.6)
+}
+}
+	
+	 invisible(p)
+}#imagem
+
+# imagem(Hotel)
