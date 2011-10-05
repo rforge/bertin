c2642dcef2d8c7550b8536ad08b359482e78eb68
gsawitzki <gsawitzki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-05 17:46:37 +0000

    attempts to stabilize imbedded palette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bertin@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/colramp.R
+++ pkg/R/colramp.R
@@ -4,6 +4,7 @@
 #$Date$
 #$Author$
 
+# colour ramp, as a separate picture. Use setup by layout.
 colramp <-
 function (col=  heat.colors(256),  nr=100, horizontal=TRUE, main,...)
 {
@@ -40,32 +41,97 @@ if (horizontal)
 # colramp(col=c("white","black"))
 
 # colour ramp imbedded in picture margin 1
-colrampmarg <- function(col=  heat.colors(256), nr=100, minlab=0, maxlab=100, main,...){
-	if (missing(main)) main<-deparse(substitute(col))
+colrampmarg5 <- function(colpalette =  palette(), vals=0:100, main = "scores",...){
+	if (missing(main)) main<-deparse(substitute(colpalette))
+	oldpar2 <-  par("plt", "mfg", "new")
+	plt <- par("plt") # x1,x2, y1, y2
+	plt[1] <- plt[1]+0.02
+	plt[2] <- plt[2]-0.02
+	plt[3] <- 0.05
+	plt[4] <- 0.08
+	par(plt=plt,  new=TRUE)
+#	oldpar1 <- par(no.readonly=TRUE)
+#	oldpar <- par(yaxt="n", plt = plt,  new=TRUE)
+	nr <- length(vals)
+ 	a <- matrix(nrow= nr, ncol= 1)
+	a[,1]<-c(1:nr)
+	# image add=FALSE: new frame. 
+	image(x= vals,, a, xlim=range(vals), col= colpalette, main="", xlab="", ylab="", xaxt="n",yaxt="n",add=TRUE,...)		
+#	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col, main="", xlab="", ylab="", xaxt="s",padj=-3, tcl=-0.2, cex.axis=0.6, yaxt="n", add=FALSE,...)		
+	title(main=main, line=0.2, cex.main=0.6)
+	axis(1,padj=-3, tcl=-0.2, cex.axis=0.6)
+#	par(oldpar1)
+#	par(mfg= oldpar1$mfg)
+par(oldpar2)
+}
+
+
+# colour ramp imbedded in picture margin 1
+colrampmarg <- function(colpalette =  palette(), vals=0:100, main,...){
+	17 + 4
+	if (missing(main)) main<-deparse(substitute(colpalette))
 	plt <- par("plt") # x1,x2, y1, y2
 	plt[1] <- plt[1]+0.02
 	plt[2] <- plt[2]-0.02
 	plt[3] <- 0.05
 	plt[4] <- 0.08
 	oldpar1 <- par(no.readonly=TRUE)
-	oldpar <- par(yaxt="n", plt = plt,  new=TRUE)#, mar=c(2,0,2,0)+0.1)#,mar=c(1,0,1,0)+0.1)#, mar=c(1,0,0,0)+0.1)plt=c(0,1,0,1), 
-#        mar=c(3.5,12.0,0.5,12.0), new=TRUE, xaxt="n", las=1)
- 	#colramp(col=col, nr=nr, horizontal=TRUE, ...)
+	oldpar <- par(yaxt="n", plt = plt,  new=TRUE)
+	nr <- length(vals)
  	a <- matrix(nrow= nr, ncol= 1)
 	a[,1]<-c(1:nr)
-#	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col,main="", xlab="", cex.axis=0.6, tcl=-0.2, padj=0, add=FALSE,...) #, tcl=0.5
-	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col, main="", xlab="", ylab="", xaxt="n",yaxt="n",add=FALSE,...) #, tcl=0.5
-	title(main=main, line=0.5, cex.main=0.6)
+	# image add=FALSE: new frame. 
+	image(x= vals,, a, xlim=range(vals), col= colpalette, main="", xlab="", ylab="", xaxt="n",yaxt="n",add=FALSE,...)		
+#	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col, main="", xlab="", ylab="", xaxt="s",padj=-3, tcl=-0.2, cex.axis=0.6, yaxt="n", add=FALSE,...)		
+	title(main=main, line=0.2, cex.main=0.6)
 	axis(1,padj=-3, tcl=-0.2, cex.axis=0.6)
-# 	mtext(minlab,at=0, side=1); 
-#	mtext(maxlab,at=100, side=1); 
- #	text(0,0,"zero",pos=2)
- #	text(100,0,"top",pos=4)
- #	str(par())
-# 	str(oldpar)
+	par(oldpar1)
+	par(mfg= oldpar1$mfg, new=FALSE)
+}
+
+
+colrampmarg3 <- function(palette =  palette(), vals =0:100, main,...){
+	if (missing(main)) main<-deparse(substitute(palette))
+	plt <- par("plt") # x1,x2, y1, y2
+	plt[1] <- plt[1]+0.02
+	plt[2] <- plt[2]-0.02
+	plt[3] <- 0.05
+	plt[4] <- 0.08
+	oldpar1 <- par(no.readonly=TRUE)
+	oldpar <- par(yaxt="n", plt = plt,  new=TRUE)
+	nr <- length(vals) # shift by 1 ?
+ 	a <- matrix(nrow= nr, ncol= 1)
+	a[,1]<-c(1:nr)
+#	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col= palette, main="", xlab="", ylab="", xaxt="n",yaxt="n",add=FALSE,...)		
+	image(x= 0:100,, a, xlim=c(0,100), col= heat.colors(256), 
+		main="", xlab="", ylab="", xaxt="s",tcl=-0.2,padj=-3, cex.axis=0.6, yaxt="n", add=FALSE,...)		
+	title(main=main, line=0.2, cex.main=0.6)
+#	axis(1,padj=-3, tcl=-0.2, cex.axis=0.6)
 	par(oldpar1)
 	par(mfg= oldpar1$mfg)
 }
+
+# colour ramp imbedded in picture margin 1
+colrampmarg2 <- function(colpalette =  palette(), nr=100, main,...){
+	if (missing(main)) main<-deparse(substitute(colpalette))
+	plt <- par("plt") # x1,x2, y1, y2
+	plt[1] <- plt[1]+0.02
+	plt[2] <- plt[2]-0.02
+	plt[3] <- 0.05
+	plt[4] <- 0.08
+	oldpar1 <- par(no.readonly=TRUE)
+	oldpar <- par(yaxt="n", plt = plt,  new=TRUE)
+ 	a <- matrix(nrow= nr, ncol= 1)
+	a[,1]<-c(1:nr)
+	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col= colpalette, main="", xlab="", ylab="", xaxt="n",yaxt="n",add=FALSE,...)		
+#	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col, main="", xlab="", ylab="", xaxt="s",padj=-3, tcl=-0.2, cex.axis=0.6, yaxt="n", add=FALSE,...)		
+	title(main=main, line=0.2, cex.main=0.6)
+	axis(1,padj=-3, tcl=-0.2, cex.axis=0.6)
+	par(oldpar1)
+	par(mfg= oldpar1$mfg)
+}
+
+
 # p <- imagem(Hotel); colrampmarg()
 
 # colrampmarg(main="")
@@ -90,7 +156,7 @@ colrampmarg1 <- function(col=  heat.colors(256), nr=100, minlab=0, maxlab=100, m
  	a <- matrix(nrow= nr, ncol= 1)
 	a[,1]<-c(1:nr)
 #	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col,main="", xlab="", cex.axis=0.6, tcl=-0.2, padj=0, add=FALSE,...) #, tcl=0.5
-	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col, main="", xlab="", ylab="", xaxt="n",yaxt="n",add=TRUE,...) #, tcl=0.5
+	image(x= (1:nr)/nr*100,, a, xlim=c(1,100), col=col, main="", xlab="", ylab="", xaxt="n",yaxt="n",add=TRUE,...) #, tcl=0.5 ## use current picture
 	title(main=main, line=0.5, cex.main=0.6)
 	axis(1,padj=-3, tcl=-0.2, cex.axis=0.6)
 	cat("par0 ");str(par())
