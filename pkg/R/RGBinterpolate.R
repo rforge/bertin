72581eb8a9535afa6261392d1965ba3979364e37
gsawitzki <gsawitzki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-11 09:10:08 +0000

    RGBinterpolate corrected
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bertin@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RGBinterpolate.R
+++ pkg/R/RGBinterpolate.R
@@ -2,7 +2,7 @@ RGBinterpolate <- function (n=100, fromRGB, toRGB, igamma=1.0){
 	 if ((n <- as.integer(n[1])) > 0) {
 		 from <-col2rgb(fromRGB)/255
 		 to <-col2rgb(toRGB)/255
-		 ifrom <- ((n:0)/n)^igamma; ito <- ((0:n)/n)^igamma
+		 ifrom <- ((n:0)/n)^igamma; ito <- 1-ifrom
 		rgb(ifrom*from[1]+ito*to[1],ifrom*from[2]+ito*to[2],ifrom*from[3]+ito*to[3])
 		 }
      else character(0)
