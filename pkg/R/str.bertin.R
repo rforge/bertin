e9e8e1bef192d2f426dcf8fc85391365583a1f6a
gsawitzki <gsawitzki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-04 20:12:04 +0000

    typos fixed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bertin@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/str.bertin.R
+++ pkg/R/str.bertin.R
@@ -1,12 +1,26 @@
 str.bertin <- function(object, ...)
 {
 	
-	cat("'bertin':	Still under construction.","\n",sep="")
+	cat("'bertin':	Still under construction. Data structure may change.","\n",sep="")
     # ## Method to 'str' for  'data.frame' objects
     # if(! is.data.frame(object)) {
 	# warning("str.data.frame() called with non-data.frame -- coercing to one.")
 	# object <- data.frame(object)
     # }
+
+cat("Main tile: ", attr(object,"main"),"\n",sep=" ")
+
+if (is.matrix(object)) {
+	cat("Dim:", dim(object),"\n",sep=" ")
+	vo <- attr(object, "var.orientation")
+	cat("Variables ",vo,"\n",sep=" ")
+
+	if (vo =="byrow") {cat("Variables: ", rownames(object),"\n",sep=" ")} else {
+		cat("variables: ", colnames(object),"\n",sep=" ")}
+	}
+
+	
+cat("\n","More information: zcol is a cache of the currently transform of the variable values.","\n",sep=" ")
 
     ## Show further classes // Assume that they do NOT have an own Method --
     ## not quite perfect ! (.Class = 'remaining classes', starting with current)
