f67b135a7b911ec8775175716c8e0f92193d2bc6
gsawitzki <gsawitzki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-16 00:38:53 +0000

    sync
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bertin@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/imagebg.R
+++
@@ -1,9 +0,0 @@
-# make a neutral background for undefined slots.
-# Note: colour should be adjusted to give a neutral grey when compared to the data points.
-imageBG <- function (x = seq(0, 1, length.out = nrow(z)), y = seq(0, 1, 
-    length.out = ncol(z)), z, zlim = range(z[is.finite(z)]), 
-    xlim = range(x), ylim = range(y), col = "#80808080", add=TRUE, ...)
-{
-	z <- ifelse(is.na(e[[k]]),1,NA)
-	image(x,y,z,zlim,xlim,ylim,col,add,...)
-}
--- pkg/R/ordergrouped.R
+++
@@ -1,9 +0,0 @@
-#$Id: ordergrouped.R 45 2011-09-23 20:12:00Z gsawitzki $
-ordergrouped <- function(..., group, na.last = TRUE, decreasing = FALSE){
-	if (missing(group))
-	{order(..., na.last = na.last, decreasing = decreasing)} else {
-		ox <- order(..., na.last = na.last, decreasing = decreasing)
-		ogro <- order(group[ox])
-		ox[ogro]
-	}
-}
